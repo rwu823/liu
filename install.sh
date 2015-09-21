ec4c13a54c13a324bc95bebcef082c97cfd38bb9
RoCky Wu <rocky_wu@trend.com.tw>
2015-09-22 07:40:36 +0800

    chnage download path to src

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/rwu823/liu/raw/master/OpenVanilla%2Bliu.zip;
+wget https://github.com/rwu823/liu/raw/master/src/OpenVanilla%2Bliu.zip;
 unzip ./OpenVanilla+liu.zip;
 mv ./OpenVanilla.app ~/Library/Input\ Methods/;
 mkdir -p ~/Library/Application\ Support/OpenVanilla/UserData/TableBased;

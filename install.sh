60747090153d7eb185daf264f5e307eaa32aa199
RoCky Wu <rocky_wu@trend.com.tw>
2015-09-21 21:11:14 +0800

    fix with mkdir -p

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 wget https://github.com/rwu823/liu/raw/master/OpenVanilla%2Bliu.zip;
 unzip ./OpenVanilla+liu.zip;
 mv ./OpenVanilla.app ~/Library/Input\ Methods/;
+mkdir -p ~/Library/Application\ Support/OpenVanilla/UserData/TableBased;
 mv ./liu7.cin ~/Library/Application\ Support/OpenVanilla/UserData/TableBased/liu7.cin;

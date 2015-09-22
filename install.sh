32d6cf483b1b3c78178d1ba52356eaa9e3c99569
RoCky Wu <rocky_wu@trend.com.tw>
2015-09-22 15:20:58 +0800

    change && replace ;

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-wget https://github.com/rwu823/liu/raw/master/src/OpenVanilla%2Bliu.zip;
-unzip ./OpenVanilla+liu.zip;
-mv ./OpenVanilla.app ~/Library/Input\ Methods/;
-mkdir -p ~/Library/Application\ Support/OpenVanilla/UserData/TableBased;
+wget https://github.com/rwu823/liu/raw/master/src/OpenVanilla%2Bliu.zip &&
+unzip ./OpenVanilla+liu.zip &&
+mv ./OpenVanilla.app ~/Library/Input\ Methods/ &&
+
+mkdir -p ~/Library/Application\ Support/OpenVanilla/UserData/TableBased &&
 mv ./liu7.cin ~/Library/Application\ Support/OpenVanilla/UserData/TableBased/liu7.cin;
 
 echo "Installed... ~/Library/Input Methods/OpenVanilla.app";

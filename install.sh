958153b517b7f62b996fd339a8ad16dc2e732ed6
anton_yu <anton_yu@askey.com>
2020-07-04 18:57:53 +0800

    * fixed script that missed  CRLF * update OpenVanilla version to v1.5.0(2019/10/29) * Use Installer instead of Install app directly * added more messages when error occurs.

--- install.sh
+++ install.sh
@@ -1,16 +1,52 @@
 #!/bin/bash
 
-curl -LO https://github.com/rwu823/liu/raw/master/src/OpenVanilla%2Bliu.zip &&
-unzip ./OpenVanilla+liu.zip &&
-mv ./OpenVanilla.app ~/Library/Input\ Methods/ &&
+PKGZIP="OpenVanilla+liu.zip"
+DIRTBL=$HOME/Library/Application\ Support/OpenVanilla/UserData/TableBased
+PATHAPP=$HOME/Library/Input\ Methods/OpenVanilla.app
+PATHTBL="$DIRTBL/liu7.cin"
 
-mkdir -p ~/Library/Application\ Support/OpenVanilla/UserData/TableBased &&
-mv ./liu7.cin ~/Library/Application\ Support/OpenVanilla/UserData/TableBased/liu7.cin;
+if [ ! -s "$PKGZIP" ]; then
+    curl -LO "https://github.com/rwu823/liu/raw/master/src/$PKGZIP"
+fi
+if [ ! -s "$PKGZIP" ]; then
+    echo "Sorry, we cannot download $PKGZIP"
+    exit 1
+fi
+if [ -d "$PATHAPP" ]; then
+    echo "We'd need to uninstall the original OpenVanilla"
+    rm -fr "$PATHAPP"
+    if [ $? -ne 0 ]; then
+        echo "Cannot remove OpenVanilla successfully"
+        exit 1
+    fi
+    rm -f "$PATHTBL"
+fi
 
-echo "Installed... ~/Library/Input Methods/OpenVanilla.app";
-echo "Installed... ~/Library/Application Support/OpenVanilla/UserData/TableBased/liu7.cin";
+mkdir -p $DIRTBL
 
-open /System/Library/PreferencePanes/Keyboard.prefPane;
+rm -fr OpenVanillaInstaller.app liu7.cin
+unzip OpenVanilla+liu.zip
+if [ $? -ne 0 ]; then
+    echo "Failed to unzip"
+    exit 2
+fi
+open ./OpenVanillaInstaller.app
+if [ $? -ne 0 ]; then
+    echo "Failed to Install OpenVanilla"
+    exit 3
+fi
+mv ./liu7.cin "$PATHTBL"
+if [ $? -ne 0 ]; then
+    echo "Failed to Install Liu Table"
+    exit 4
+fi
+if [ -s "$PATHTBL" ]; then
+    echo "OpenVanilla Installed"
+    echo "liu7.cin Installed"
+    open /System/Library/PreferencePanes/Keyboard.prefPane
+    rm -f "$PKGZIP"
+else
+    echo "Sorry, but there is something wrong"
+fi
 
-# remove the .zip file
-rm ./OpenVanilla%2Bliu.zip;
+
